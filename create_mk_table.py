996510e1e701afce10cad07d9b4987a954643133
bogdan.crisan <bogdan.18rap@gmail.com>
2018-11-12 19:46:23 +0200

    Added documentation to the code functions.

--- create_mk_table.py
+++ create_mk_table.py
@@ -1,7 +1,7 @@
 import os, json
+import time
 from os import listdir
 from os.path import isfile, join
-from pprint import pprint
 from datetime import datetime
 
 
@@ -27,6 +27,11 @@ def hg_timestamps_handler(timestamp, timezone):
 
 
 def clear_file(file_name):
+    """
+    This function takes a file that clears the content and output's a base table header for a markdown file.
+    :param file_name: Name of the file to be written. (should also contain the path)
+    :return: A file should be created and should contain base table.
+    """
     f = open(file_name, 'w')
     base_table = '|      Repository      |                   Last commit               |    Deploy time       | \n' + \
                  '|:--------------------:|:-------------------------------------------:|:--------------------:| \n'
@@ -35,7 +40,15 @@ def clear_file(file_name):
 
 
 def write_main_mk_table(file_name, repository, last_commit, deploy_time):
-
+    """
+    This function opens a file (that file should be already created and should contain a markdown table header) and
+    appends to it a row that will contain the repository, the last commit and the deploy time.
+    :param file_name: Name of the file in which the content is appended. (should also contain the path)
+    :param repository: Repository name for the first element of the table.
+    :param last_commit: Description of the last commit used as the 2nd element of the table
+    :param deploy_time: Time and Time designator used as the 3rd element of the table
+    :return:
+    """
     row = "|" + repository + \
           "|" + last_commit + \
           "|" + deploy_time + \
@@ -46,6 +59,13 @@ def write_main_mk_table(file_name, repository, last_commit, deploy_time):
 
 def generate_main_mk_table():
     """
+    Function that handles and generates the markdown table.
+    The function starts looking into the /repositories folder after json files.
+    Once those are found, and stored into list and after that we get every element from the list (each element being a
+    json file name at this point) and used to open that file, and grab the commit description, commit date and the
+    repository name that luckly is the same as the file name.
+    The function also generates the github_base_link that is used as a link in the markdown table.
+    :return:
     """
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -68,6 +88,7 @@ def generate_main_mk_table():
             # pprint(data)
             github_base_link = "https://github.com/Akhliskun/firefox-infra-changelog/blob/master/repositories/"
             repository_name = "[" + f.rstrip().replace(".json", "") + "]" + "(" + github_base_link + f.rstrip().replace(" ", "%20") + ")"
+
             for test in data["changesets"]:
                 commit_description = test["desc"]
                 commit_description = commit_description.rstrip('\r\n').replace('\n', '')
@@ -78,6 +99,7 @@ def generate_main_mk_table():
                 data_push = hg_timestamps_handler(test, time_designator)
                 commit_description = str(commit_description)
                 write_main_mk_table("main_mk_table.md", repository_name, commit_description, data_push)
+                # We are braking this for loop since we got the last commit.
                 break
 
 
